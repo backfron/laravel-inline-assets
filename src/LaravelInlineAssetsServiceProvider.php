5568a6d69f69852bc94b10c9f64f855619f5ab07
Jairo Ushiña <jago86@gmail.com>
2021-11-09 11:01:11 -0500

    Clean commented code.

--- src/LaravelInlineAssetsServiceProvider.php
+++ src/LaravelInlineAssetsServiceProvider.php
@@ -15,11 +15,6 @@ class LaravelInlineAssetsServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'backfron');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'backfron');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
             $this->bootForConsole();
@@ -121,23 +116,5 @@ class LaravelInlineAssetsServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/laravel-inline-assets.php' => config_path('laravel-inline-assets.php'),
         ], 'laravel-inline-assets.config');
-
-        // Publishing the views.
-        /*$this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/backfron'),
-        ], 'laravel-inline-assets.views');*/
-
-        // Publishing assets.
-        /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/backfron'),
-        ], 'laravel-inline-assets.views');*/
-
-        // Publishing the translation files.
-        /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/backfron'),
-        ], 'laravel-inline-assets.views');*/
-
-        // Registering package commands.
-        // $this->commands([]);
     }
 }
